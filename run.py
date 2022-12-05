33c9b492b1bc5c2afe717f32be4b3627322c0749
Ian-Garrigan <igarrigan@hotmail.com>
2022-12-05 08:34:06 +0000

    Create game intro & style with ascii

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import libraries needed
+
+from random import randint
+import os
+import sys
+
+
+os.system("clear")
+print("  ___  ___ ______________   ____  ______ _________  ____")
+print(" / _ )/ _ /_  __/_  __/ /  / __/ / __/ // /  _/ _ \\/ __/")
+print("/ _  / __ |/ /   / / / /__/ _/  _\\ \\/ _  // // ___/\\ \\ ")
+print("/____/_/ |_/_/   /_/ /____/___/ /___/_//_/___/_/  /___/ ")
+print("")
+print("        Get ready for BATTLESHIP! Good luck soldier.")
+print("   Typically this game is played on a 10 x 10 grid.")
+print("      But you can opt for a smaller game board.")
+print("   Please press Enter after each input.")
+print("\u001b[31m---------------------------------------------------\033[0;0m")

08e3103da310ba300c2f866928aa6e62b6bba40f
Ian-Garrigan <igarrigan@hotmail.com>
2022-12-05 08:38:00 +0000

    User instruction intro & require grid size

--- run.py
+++ run.py
@@ -16,3 +16,17 @@ print("   Typically this game is played on a 10 x 10 grid.")
 print("      But you can opt for a smaller game board.")
 print("   Please press Enter after each input.")
 print("\u001b[31m---------------------------------------------------\033[0;0m")
+while True:
+    BATTLE_AREA = input(" Enter battle area size (range 1-10): ")
+    if BATTLE_AREA.isdigit():
+        BATTLE_AREA = int(BATTLE_AREA)
+        if BATTLE_AREA > 1 and BATTLE_AREA <= 10:
+            print(" ")
+            print("Lets Rock n Roll kid, you know what to do.")
+            print("-------------------------------------------------------")
+            break
+        else:
+            print(" Please pick a number ranging from 1 to 10")
+    else:
+        print("No letters soldier and keep it less than 11!.")
+        continue
